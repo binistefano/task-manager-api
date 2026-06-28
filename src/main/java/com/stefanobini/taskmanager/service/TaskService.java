cbaf194bce0145edac2290531d2ba7a6ea1522f4
Stefano Bini <165495118+binistefano@users.noreply.github.com>
2026-06-28 18:35:25 +0200

    feat: add pagination support

--- src/main/java/com/stefanobini/taskmanager/service/TaskService.java
+++ src/main/java/com/stefanobini/taskmanager/service/TaskService.java
@@ -3,13 +3,15 @@ package com.stefanobini.taskmanager.service;
 import com.stefanobini.taskmanager.dto.TaskRequest;
 import com.stefanobini.taskmanager.dto.TaskResponse;
 import com.stefanobini.taskmanager.entity.TaskStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface TaskService {
     TaskResponse createTask(TaskRequest request);
 
-    List<TaskResponse> getAllTasks();
+    Page<TaskResponse> getAllTasks(Pageable pageable);
 
     TaskResponse getTaskById(Long id);
 
