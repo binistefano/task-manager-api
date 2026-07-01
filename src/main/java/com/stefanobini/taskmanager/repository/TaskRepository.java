9de1f69b7fddc73bb7162d2a44abd0f6a74cb090
Stefano Bini <165495118+binistefano@users.noreply.github.com>
2026-07-01 03:32:11 +0200

    feat: add task specification to filter by status and title

--- src/main/java/com/stefanobini/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/stefanobini/taskmanager/repository/TaskRepository.java
@@ -5,12 +5,13 @@ import com.stefanobini.taskmanager.entity.TaskStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    Page<Task> findByStatus(TaskStatus status, Pageable pageable);
+public interface TaskRepository extends JpaRepository<Task, Long>,
+        JpaSpecificationExecutor<Task> {
 
 }
